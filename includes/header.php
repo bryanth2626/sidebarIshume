275f277b3e7c4a6b8bd1eed349ed9ccd5f95b058
Brayan Tasayco <brayantasaycohuaman@gmail.com>
2026-05-10 23:35:39 -0600

    implementacion de estilo de calendario en mi entidad Eventos

--- includes/header.php
+++ includes/header.php
@@ -10,8 +10,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 <!-- Estilos del calendario -->
-<link rel="stylesheet" href="style/calendar.css">
-<link rel="stylesheet" href="style/estilos.css">
+<link rel="stylesheet" href="css/calendar.css">
 <!-- CSS GLOBAL -->
 <link rel="stylesheet" href="css/main.css">
 
